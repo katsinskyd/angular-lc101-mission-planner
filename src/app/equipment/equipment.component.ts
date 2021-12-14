5756e5f20370cbeb68ae0438621a35282c1934b4
Kat <kathryn.garnier@gmail.com>
2021-12-13 20:24:32 -0500

    finished part 1

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -27,5 +27,11 @@ export class EquipmentComponent implements OnInit {
    ngOnInit() { }
 
    // Code your addItem function here:
-   
+   addItem(equipment: object) {
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment["mass"];
+    if ((this.cargoMass - this.maximumAllowedMass) <= 200) {
+      return true;
+    } 
+  }
 }
